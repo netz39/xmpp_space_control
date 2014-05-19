c1a285a888b616e93c76c9b3bb70038b43471ed4
Tux <tux@netz39.de>
2014-05-19 16:11:19 +0200

    I2C Endpoint design and documentation.
    The design is strongly related to the wiringPi I²C library (which is the target library), but can be used elsewhere, too.

--- i3c_client/i2cendpoint.h
+++ i3c_client/i2cendpoint.h
@@ -16,11 +16,145 @@
 #ifndef I2CENDPOINT_H__
 #define I2CENDPOINT_H__
 
+
+#include <stdexcept>
+#include <string>
+
 namespace xmppsc {
-  
-  
-  
-  
+
+//! Exception during I2C communication via an endpoint.
+class I2CEndpointException : public std::exception {
+    //! Create a new exception.
+    /*!
+     * @param address Device address of the I2C peer.
+     * @param errno The errno from the backing I2C library
+     * @param const msg an error message if available
+     */
+    I2CEndpointException(const int address, const int error, const std::string msg);
+
+    virtual ~I2CEndpointException() throw();
+
+    //! Get the device address corresponding to this exception.
+    virtual const int address() throw();
+
+    //! Get the error (errno) corresponding to this exception.
+    virtual const int error() throw();
+
+    //! Get the exception message.
+    /*!
+     * \returns the exception message
+     */
+    virtual const char* what() const throw();
+private:
+    const int m_address;
+    const int m_error;
+    const std::string m_what;
+};
+
+//! I2C communication endpoint
+/*!
+ * I2C endpoint representing a single device on the bus.
+ *
+ * An endpoint must be setup in order to be used. Afterwards read and write
+ * operations can be arbitrarily used. On errors an I2CEndpointException is thrown.
+ *
+ * This design is strongly related to the wiringPi I2C library, but can be used for
+ * other implementations, too.
+ *
+ * See http://wiringpi.com/reference/i2c-library/ for details. Most documentation
+ * is just copied from there.
+ */
+class I2CEndpoint {
+public:
+    //! Create an endpoint instance for a specific address.
+    /*!
+     * @param address The I2C address of the target device.
+     * @throws std::out_of_range if the address is not a valid I2C address
+     */
+    // TODO check parameter type, 8 bits are sufficient
+    I2CEndpoint(const int address) throw (std::out_of_range);
+
+    //! Return the address for this endpoint.
+    /*!
+     * @returns The endpoint device address.
+     */
+    const int address() throw();
+
+    //! Set up the I2C connection for further communication.
+    /*!
+     * This initialises the I2C system with your given device identifier.
+     * The ID is the I2C number of the device and you can use the
+     * i2cdetect program to find this out.
+     *
+     * @throws I2CEndpointException if setup fails.
+     */
+    void setup() throw(I2CEndpointException);
+
+    //! Close the connection to the I2C device and cleanup all resources.
+    /*!
+     * @throws I2CEndpointException if the cleanup fails.
+     */
+    void close() throw(I2CEndpointException);
+
+    //! Simple device read_reg_16
+    /*!
+     * Some devices present data when you read them without having to do any register transactions.
+     *
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int read() throw(I2CEndpointException);
+    //! Simple device write.
+    /*!
+     * Some devices accept data this way without needing to access any internal registers.
+     *
+     * @param data The data to be written.
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int write(int data) throw(I2CEndpointException);
+
+    //! Read 8 bits of data from a device register.
+    /*!
+     * @param reg The device register.
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int read_reg_8(int reg) throw(I2CEndpointException);
+    //! Read 16 bits of data from a device register.
+    /*!
+     * @param reg The device register.
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int read_reg_16(int reg) throw(I2CEndpointException);
+
+    //! Write 8 bits of data to a device register.
+    /*!
+     * @param reg The device register.
+     * @param data The data.
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int write_reg_8(int reg, int data) throw(I2CEndpointException);
+    //! Write 16 bits of data to a device register.
+    /*!
+     * @param reg The device register.
+     * @param data The data.
+     * @returns Result from the I2C call.
+     * @throws I2CEndpointException if access to the I2C device fails.
+     */
+    int write_reg_16(int reg, int data) throw(I2CEndpointException);
+
+protected:
+    const int _fd() throw();
+
+private:
+    const int m_address;
+    int m_fd;
+};
+
+
 }
 
 
