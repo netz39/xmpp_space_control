75ee7f6c761cbb92e400c6f59429ed1dbd9e3e48
Tux <tux@netz39.de>
2014-05-19 16:33:10 +0200

    I2C endpoint: implementation if I2CEndpointException

--- i3c_client/i2cendpoint.cpp
+++ i3c_client/i2cendpoint.cpp
@@ -21,5 +21,25 @@
 using namespace xmppsc;
 
 
+I2CEndpointException::I2CEndpointException(const int address, const int error, const std::string msg)
+    : m_address(address), m_error(error), m_what(msg) { }
+
+I2CEndpointException::~I2CEndpointException() throw() {}
+
+const int I2CEndpointException::address() throw() {
+    return m_address;
+}
+
+const int I2CEndpointException::error() throw()
+{
+    return m_error;
+}
+
+const char* I2CEndpointException::what() const throw()
+{
+    return m_what.c_str();
+}
+
+
 
 // End of file
