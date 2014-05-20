22369ff74d7453be4a2867dc028f3a7e92d72060
Tux <tux@netz39.de>
2014-05-20 17:03:58 +0200

    disable the beeping…

--- i3c_client/main.cpp
+++ i3c_client/main.cpp
@@ -42,9 +42,9 @@ void TestMethod::handleSpaceCommand(gloox::JID peer, xmppsc::SpaceCommand sc, xm
 int main(int argc, char **argv) {
     xmppsc::I2CEndpointBroker*  broker = new xmppsc::I2CEndpointBroker();
 
-    xmppsc::I2CEndpoint* ep = broker->endpoint(0x22);
+    //xmppsc::I2CEndpoint* ep = broker->endpoint(0x22);
     // Send a double beep
-    std::cout << std::hex << "0x" << ep->read_reg_16(0x95) << std::endl;
+    //std::cout << std::hex << "0x" << ep->read_reg_16(0x95) << std::endl;
 
     gloox::Client* client=0;
     try {
