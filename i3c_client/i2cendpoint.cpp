8a990b6eb7dd2a589895d0c79492788c73fd4f98
Tux <tux@netz39.de>
2014-05-20 15:47:09 +0200

    Use find to access map members.

--- i3c_client/i2cendpoint.cpp
+++ i3c_client/i2cendpoint.cpp
@@ -56,23 +56,25 @@ I2CEndpointBroker::~I2CEndpointBroker() throw(I2CEndpointException)
 
 I2CEndpoint& I2CEndpointBroker::endpoint(const int address) throw(I2CEndpointException, std::out_of_range)
 {
-    try {
-        // try to get endpoint from the map
-        return endpoints.at(address);
-    } catch (std::out_of_range& oor) {
+    // try to get endpoint from the map
+    endpoint_map::iterator it = endpoints.find(address);
+
+    if (it == endpoints.end()) {
         // none found, create and setup
         I2CEndpoint ep(address);
         ep.setup();
 
         // store and return
-	std::pair<endpoint_map::iterator, bool> res =
-	  endpoints.insert(endpoint_map::value_type(address, ep));
-	  
-	  if (!res.second)
-	    throw I2CEndpointException(address, 0, "Could not store endpoint to broker!");
-	  
-	  return res.first->second;
+        std::pair<endpoint_map::iterator, bool> res =
+            endpoints.insert(endpoint_map::value_type(address, ep));
+
+        if (!res.second)
+            throw I2CEndpointException(address, 0, "Could not store endpoint to broker!");
+
+        return res.first->second;
     }
+    
+    return it->second;
 }
 
 void I2CEndpointBroker::free_all_endpoints() throw(I2CEndpointException)
