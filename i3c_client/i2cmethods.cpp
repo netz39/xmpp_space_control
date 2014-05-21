b57727d814882bb8a4c88b78ad9cc6eb261c1933
Tux <tux@netz39.de>
2014-05-21 16:51:22 +0200

    nicer output for the I2C method log

--- i3c_client/i2cmethods.cpp
+++ i3c_client/i2cmethods.cpp
@@ -110,7 +110,7 @@ void I2CReadMethod::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceComma
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom read
-    std::cout << "Perform I2C read on device " << device << "." << std::endl;
+    std::cout << "Perform I2C read on device 0x" << device << "." << std::endl;
     const int result = ep->read();
 
     // send result
@@ -137,7 +137,7 @@ void I2CRead8Method::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceComm
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom read
-    std::cout << "Perform I2C read on 8-bit register " << std::hex << reg << " of device " << device << "." << std::endl;
+    std::cout << "Perform I2C read on 8-bit register 0x" << std::hex << reg << " of device 0x" << device << "." << std::endl;
     const int result = ep->read_reg_8(reg);
 
     // send result
@@ -165,7 +165,7 @@ void I2CRead16Method::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceCom
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom read
-    std::cout << "Perform I2C read on 16-bit register " << std::hex << reg << " of device " << device << "." << std::endl;
+    std::cout << "Perform I2C read on 16-bit register 0x" << std::hex << reg << " of device 0x" << device << "." << std::endl;
     const int result = ep->read_reg_16(reg);
 
     // send result
@@ -193,7 +193,7 @@ void I2CWriteMethod::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceComm
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom write
-    std::cout << "Perform I2C write of value " << std::hex << data << " to device " << device << "." << std::endl;
+    std::cout << "Perform I2C write of value 0x" << std::hex << data << " to device 0x" << device << "." << std::endl;
     const int result = ep->write(data);
 
     xmppsc::SpaceCommand::space_command_params params;
@@ -221,7 +221,7 @@ void I2CWrite8Method::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceCom
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom write
-    std::cout << "Perform I2C 8-bit write of value " << std::hex << data << " to register " << reg << " of device " << device << "." << std::endl;
+    std::cout << "Perform I2C 8-bit write of value 0x" << std::hex << data << " to register 0x" << reg << " of device 0x" << device << "." << std::endl;
     const int result = ep->write_reg_8(reg, data);
 
     xmppsc::SpaceCommand::space_command_params params;
@@ -250,7 +250,7 @@ void I2CWrite16Method::handleSpaceCommand(gloox::JID peer, const xmppsc::SpaceCo
     I2CEndpoint* ep = broker()->endpoint(device);
 
     // perfom write
-    std::cout << "Perform I2C 16-bit write of value " << std::hex << data << " to register " << reg << " of device " << device << "." << std::endl;
+    std::cout << "Perform I2C 16-bit write of value 0x" << std::hex << data << " to register 0x" << reg << " of device 0x" << device << "." << std::endl;
     const int result = ep->write_reg_16(reg, data);
 
     xmppsc::SpaceCommand::space_command_params params;
