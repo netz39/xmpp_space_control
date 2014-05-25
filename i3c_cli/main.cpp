fa84a389bc99fe359f4d44408726317ef7e716d0
Tux <tux@netz39.de>
2014-05-25 15:15:57 +0200

    I3C CLI Client: Start client in a different thread.

--- i3c_cli/main.cpp
+++ i3c_cli/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <thread>
+
+#include <unistd.h>
 
 #include "../space_control/configuredclientfactory.h"
 #include "../space_control/spacecontrolclient.h"
@@ -6,6 +9,15 @@
 
 #include "../i3c_client/i2cmethods.h"
 
+void run_client(gloox::Client* client) {
+    std::cout << "Connecting GLOOX client." << std::endl;
+    if (!client->connect(true)) {
+        std::cerr << "could not connect!" << std::endl;
+        return;
+    }
+
+    std::cout << "Client connection finished." << std::endl;
+}
 
 int main(int argc, char **argv) {
     gloox::Client* client=0;
@@ -25,8 +37,34 @@ int main(int argc, char **argv) {
         xmppsc::SpaceControlClient* scc = new xmppsc::SpaceControlClient(client, i2ch,
                 new xmppsc::TextSpaceCommandSerializer(), af);
 
-        if (!client->connect(true))
-            std::cerr << "could not connect!" << std::endl;
+        // connect
+        std::thread client_thread(run_client, client);
+
+        while (client->state() != gloox::ConnectionState::StateConnected)
+            sleep(1);
+
+	std::cout << "Connected." << std::endl;
+
+        xmppsc::SpaceCommand::space_command_params parms;
+        parms["device"] = "0x22";
+        parms["command"] = "0x1";
+        parms["data"] = "0x5";
+        const xmppsc::SpaceCommand sc("i3c.call", parms);
+
+        xmppsc::SpaceCommandSink* sink = scc->create_sink(gloox::JID("i2c@platon"), "cli_thread");
+
+        sink->sendSpaceCommand(sc);
+
+        // wait for response
+        sleep(1);
+
+        delete sink;
+
+        std::cout << "Disconnecct." << std::endl;
+        client->disconnect();
+
+        // wait for client thread
+        client_thread.join();
 
         delete client;
     }
